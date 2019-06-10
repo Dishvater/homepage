5c5a3d34d1761d8cf5666f2e1d5eefe544add89f
Wojciech Wawrzyniak <wwawrzyniak@gmail.com>
2019-06-10 21:21:32 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+console.log('siema!');
+const name = 'wojtek';
+const age = 33;
+console.log(name);
+console.log(age);
+console.log(`Nazywam się ${name} i mam ${age} lata`);
+console.log(`${name} ${age}`);
+alert('Witajcie na mojej stronie');
