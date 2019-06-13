cb55ec21920f8f508d55c3d7b25b4391e598ab0d
Wojciech Wawrzyniak <wwawrzyniak@gmail.com>
2019-06-13 22:10:23 +0100

    feat: bem & dom

--- main.js
+++ main.js
@@ -1,8 +1,2 @@
-console.log('siema!');
-const name = 'wojtek';
-const age = 33;
-console.log(name);
-console.log(age);
-console.log(`Nazywam się ${name} i mam ${age} lata`);
-console.log(`${name} ${age}`);
-alert('Witajcie na mojej stronie');
+const heading = document.querySelector('.heading');
+console.log(heading);
